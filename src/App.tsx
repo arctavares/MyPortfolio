9751229c92d4fb068c839067b54c69f682334bba
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-08 19:50:14 -0300

    feat: change font-family, add color and hover color to li

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path='/' element={ <Home/> } />
-      
+        <Route path='/' element={ <Home/> } />
       </Routes>
     </div>
   );
