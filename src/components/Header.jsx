31b0d6d85cfca2f0c037dbc145077b90baf18024
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-08 02:49:17 -0300

    feat: add nav to header, and organize with css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,14 @@ export default function Header () {
     return (
         <div className="headerContainer">
             <div className="logoContainer"><h1>Logo</h1></div>
-            <div className="navContainer">nav</div>
+            <div className="navContainer">
+                <ul>
+                    <li>1 - About</li>
+                    <li>2 - Experience</li>
+                    <li>3 - Work</li>
+                    <li>4 - Contact</li>
+                </ul>
+            </div>
         </div>
     )
 }
