41a962df836e94badce637282b4c088bbc66d787
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-08 20:17:49 -0300

    feat: add background color to whole page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,13 @@ import Header from '../components/Header';
 
 function Home() {
   return (
-    <div className="homeContainer">
-        <Header />
-        <h1>Meu portifolio</h1>
-    </div>
+    <>
+      <Header />
+      <div className="homeContainer">
+          
+          <h1>Meu portifolio</h1>
+      </div>
+    </>
   );
 }
 
