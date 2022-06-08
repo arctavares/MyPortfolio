83f90b5aabd9cffe98b4de2e9eaca18aa6dcc79a
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-08 19:31:24 -0300

    feat: add style to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ export default function Header () {
             <div className="logoContainer"><h1>Logo</h1></div>
             <div className="navContainer">
                 <ul>
-                    <li>1 - About</li>
-                    <li>2 - Experience</li>
-                    <li>3 - Work</li>
-                    <li>4 - Contact</li>
+                    <li><span className="index">1 -</span> About</li>
+                    <li><span className="index">2 -</span> Experience</li>
+                    <li><span className="index">3 -</span> Work</li>
+                    <li><span className="index">4 -</span> Contact</li>
                 </ul>
             </div>
         </div>
