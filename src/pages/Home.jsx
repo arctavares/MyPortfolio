0dad10b3a2131dfb24107744601546a17a18e817
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-08 02:22:10 -0300

    feat: create header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Home() {
   return (
-    <div>
+    <div className="homeContainer">
+        <Header />
         <h1>Meu portifolio</h1>
     </div>
   );
