7301734d706a37e84dd6aa53eb1dd07ec8883ea1
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-07 22:54:33 -0300

    Updates

---
+++ precache-manifest.1b76d226e2a212ef10d19105a54183d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "063cae2cfb699d89c603",
+    "url": "myPortfolio/static/css/main.3d4b217f.chunk.css"
+  },
+  {
+    "revision": "063cae2cfb699d89c603",
+    "url": "myPortfolio/static/js/main.d462b480.chunk.js"
+  },
+  {
+    "revision": "1957fc38477b0fc41707",
+    "url": "myPortfolio/static/js/runtime~main.d5dc99a9.js"
+  },
+  {
+    "revision": "17cffce5b09147115348",
+    "url": "myPortfolio/static/js/2.671a7437.chunk.js"
+  },
+  {
+    "revision": "7b4b590765ba9cc9d7de",
+    "url": "myPortfolio/static/js/3.f1bbbdc7.chunk.js"
+  },
+  {
+    "revision": "ea20bf42aa64bdacb19655096fec1d10",
+    "url": "myPortfolio/index.html"
+  }
+];
