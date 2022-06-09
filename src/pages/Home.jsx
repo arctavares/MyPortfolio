b3851243a1ece2657f3f53eacc779be9302cd319
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-08 21:08:27 -0300

    feat: add personal introduction to page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../css/Home.css';
 
 function Home() {
   return (
     <>
       <Header />
       <div className="homeContainer">
-          
-          <h1>Meu portifolio</h1>
+          <div className="introContainer">
+            <p className="introduction">Hi, my name is</p>
+            <p className="myName">Armando Tavares.</p>
+            <p className="introductionText">I'am a fullstack developer junior currently studying on <span className="trybe">Trybe</span></p>
+          </div>
       </div>
     </>
   );
