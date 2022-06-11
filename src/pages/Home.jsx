f6c458629faebda619d740623d6e9bc81623782f
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-10 21:45:08 -0300

    feat: add estilization to name and carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,15 @@ import Projects from '../components/Projects';
 import '../css/Home.css';
 
 function Home() {
+  const openParenthesis = <span >{`{`}</span>;
+  const closeParenthesis = <span className="closeParenthesis">{`}`}</span>;
   return (
     <div className='body'>
       <Header />
       <div className="homeContainer">
           <div className="introContainer">
-            <p className="introduction">Hi, my name is</p>
-            <p className="myName">Armando Tavares.</p>
+            <p className="introduction">{openParenthesis} Hi, my name is :</p>
+            <p className="myName">"Armando Tavares" {closeParenthesis}</p>
             <p className="introductionText">I'am a fullstack developer junior currently studying on <span className="trybe">Trybe</span></p>
           </div>
       </div>
