f583f8d62d3d1695f8eeb2e13365a54ca954dd86
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-14 04:47:44 -0300

    feat: add experience page - in progress

---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/Experience.css';
+
+export default function Experience () {
+    return (
+        <div className="ExperienceContainer">
+            <h1 className="ExperienceTitle">Experiencia</h1>
+            <div className="experienceDescriptionContainer">
+                <p>De 2016 a 2019 - Cursei Engenharia da computação na Universidade Federal de Pelotas sem concluir </p>
+                <p>De 2021 a 2022 - Cursei o curso de desenvolvimento web fullstack na Trybe</p>
+            </div>
+        </div>
+    )
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import About from '../components/About';
 import Projects from '../components/Projects';
+import Experience from '../components/Experience';
 import '../css/Home.css';
 
 function Home() {
@@ -19,6 +20,7 @@ function Home() {
       </div>
       <About />
       <Projects />
+      <Experience />
     </div>
   );
 }
