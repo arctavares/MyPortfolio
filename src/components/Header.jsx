5f93bb03608b38d9e1f0cfe4a09e0acad4ee82b2
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-13 22:38:39 -0300

    feat: change color of my projects, change experience to projects, remove border

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header () {
             <div className="navContainer">
                 <ul>
                     <li><span className="index">1 -</span> About</li>
-                    <li><span className="index">2 -</span> Experience</li>
+                    <li><span className="index">2 -</span> Projects</li>
                     <li><span className="index">3 -</span> Work</li>
                     <li><span className="index">4 -</span> Contact</li>
                 </ul>
