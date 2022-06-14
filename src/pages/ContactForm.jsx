e26aff9a7988e8953459e2eccb04012dc9a18453
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-14 08:46:14 -0300

    feat: add structure to contact form

--- src/pages/ContactForm.jsx
+++ src/pages/ContactForm.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import Header from '../components/Header';
+import '../css/Contact.css';
 
-export default function ContactForm () {
-    return (
-        <div className="contactFormContainer">
-            <h1>Contact</h1>
+export default function ContactForm() {
+  return (
+    <>
+      <Header />
+      <div className='contactBody'>
+        <div className='contactFormContainer'>
+          <form>
+            <div class='form-row'>
+              <div class='col'>
+                <input
+                  type='text'
+                  class='form-control'
+                  placeholder='name'
+                />
+              </div>
+            </div>
+            <div class='form-group'>
+              <label for='exampleFormControlInput1'>E-mail</label>
+              <input
+                type='email'
+                class='form-control'
+                id='exampleFormControlInput1'
+                placeholder='email@example.com'
+              />
+            </div>
+            <div class='form-group'>
+              <label for='exampleFormControlTextarea1'>
+                Send me a message :)
+              </label>
+              <textarea
+                class='form-control'
+                id='exampleFormControlTextarea1'
+                rows='3'
+              ></textarea>
+            </div>
+            <button type='button' class='btn btn-success'>
+              Enviar
+            </button>
+          </form>
         </div>
-    )
-};
+      </div>
+    </>
+  );
+}
