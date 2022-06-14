73c4edd813fd55e12612aaa044ed4938b7e092b8
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-14 08:04:41 -0300

    feat: add link to footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,12 @@ import github from '../images/github.svg';
 export default function Footer() {
   return (
     <div className='footerContainer'>
-      <img src={linkedin} alt='linkedin logo' />
-      <img src={github} alt='github logo' />
+      <a href='https://www.linkedin.com/in/armando-tavares/' target="_blank" rel="noreferrer">
+        <img src={linkedin} alt='linkedin logo' />
+      </a>
+      <a href='https://github.com/arctavares' target="_blank" rel="noreferrer">
+        <img src={github} alt='github logo' />
+      </a>
     </div>
   );
 }
