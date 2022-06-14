5f226f8f8fa8d1d71c38ccfd5659bc666a7c69f4
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-14 09:20:39 -0300

    feat: add my about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import '../css/About.css';
 
-export default function About () {
-    return (
-        <div className="aboutContainer">
-            <h1 className="subTitle">About Me</h1>
-            <p className='aboutText'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
-                desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
-    )
-};
+export default function About() {
+  return (
+    <div className='aboutContainer'>
+      <h1 className='subTitle'>About Me</h1>
+      <p className='aboutText'>
+        I love development of software, the most enthusiastic thing to me is to bring ideas to life,
+        my knowledge in programming is mainly JS, React, Node.js, Mysql, Docker and currently learning more,
+        my passion is to learn something every day, i am always seeking for knowledge.
+        Powered by VS Code and caffeine.
+      </p>
+    </div>
+  );
+}
