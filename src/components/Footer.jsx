37fca142e118d576a2171370679fabe7e9f93dd9
Armando - PC principal Ubuntu <cosilk@hotmail.com>
2022-06-14 07:19:11 -0300

    feat: add footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../css/Footer.css';
+import linkedin from '../images/linkedin.svg';
+import github from '../images/github.svg';
 
-export default function Footer () {
-    return (
-        <div className="footerContainer">
-            <h1 className="footerText">Footer</h1>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className='footerContainer'>
+      <img src={linkedin} alt='linkedin logo' />
+      <img src={github} alt='github logo' />
+    </div>
+  );
 }
